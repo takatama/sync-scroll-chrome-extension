ed65c45e5f940bde68493269c7b6bd23fae028bd
Hirokazu Takatama <hirokazu.takatama@gmail.com>
2012-05-03 06:17:31 +0900

    first commit

---
+++ sync_scroll/background.js
@@ -0,0 +1,79 @@
+﻿sync_scroll = {
+    on: true,
+    setOn: function (on) {
+        sync_scroll.on = on;
+        if(sync_scroll.isOn()) {
+            chrome.browserAction.setBadgeText({
+                text: 'on'
+            });
+        } else {
+            chrome.browserAction.setBadgeText({
+                text: 'off'
+            });
+        }
+    },
+    isOn: function () {
+        return sync_scroll.on;
+    },
+    toggle: function () {
+        sync_scroll.setOn(!sync_scroll.isOn());
+    },
+    current_id: null,
+    current_index: null
+}
+
+chrome.browserAction.onClicked.addListener(function (){
+    sync_scroll.toggle();
+});
+
+var selectedTabs = {};
+
+chrome.tabs.onHighlighted.addListener(function (highlightedInfo) {
+	selectedTabs[highlightedInfo.windowId] = highlightedInfo.tabIds;
+	console.log('highlighted on windowId:' + highlightedInfo.windowId + ' tabIds:' + highlightedInfo.tabIds.join(','));
+});
+
+var ports = {};
+
+chrome.extension.onConnect.addListener(function (port) {
+	console.assert(port.name === 'sync_scroll');
+	var tab_id = port.sender.tab.id;
+	ports[tab_id] = port;
+	console.log('port is connected from tabId:' + tab_id);
+	port.onMessage.addListener(function emit(msg) {
+		if (!sync_scroll.isOn()) {
+			return;
+		}
+		if (msg.window_scrollY) {
+			var x = msg.window_scrollX;
+			var y = msg.window_scrollY;
+			console.log('background receives scrollXY:' + x + ',' + y);
+			sendToSelectedTabs(port, msg);
+		}
+	});
+});
+
+function selectedTabIds() {
+	var tabIds = [],
+		window_id;
+	for (window_id in selectedTabs) {
+		if (selectedTabs.hasOwnProperty(window_id)) {
+			tabIds.push(selectedTabs[window_id]);
+		}
+	}
+	return tabIds;
+}
+
+function sendToSelectedTabs(port, msg) {
+	var tabIds = selectedTabIds(),
+		i;
+	for (i = 0; i < tabIds.length; i++) {
+		var tab_id = tabIds[i];
+		if (ports[tab_id] && ports[tab_id] !== port) {
+			ports[tab_id].postMessage(msg);
+			console.log('background sends ' + msg.window_scrollX + ',' + msg.window_scrollY + ' to tabId:' + tab_id);
+		}
+	}
+}
+
+sync_scroll.setOn(true);
---
+++ sync_scroll/scroll.js
@@ -0,0 +1,37 @@
+﻿sync_scroll = {
+	port: chrome.extension.connect({name: "sync_scroll"}),
+	focused: true
+}
+
+window.addEventListener('scroll', function () {
+	if(!sync_scroll.focused) {
+		return;
+	}
+	var x = window.scrollX;
+	var y = window.scrollY;
+	console.log('tab sends scrollXY:' + x + ',' + y);
+	sync_scroll.port.postMessage({
+		window_scrollX: x,
+		window_scrollY: y
+	});
+});
+
+window.addEventListener('focus', function () {
+	console.log('tab onfocus');
+	sync_scroll.focused = true;
+});
+
+window.addEventListener('blur', function () {
+	console.log('tab onblur');
+	sync_scroll.focused = false;
+});
+
+
+sync_scroll.port.onMessage.addListener(function (msg) {
+	if (msg.window_scrollY && !sync_scroll.focused) {
+		var x = msg.window_scrollX;
+		var y = msg.window_scrollY;
+		console.log('tab receives scrollXY:' + x + ',' + y);
+		window.scroll(x, y);
+	}
+});
